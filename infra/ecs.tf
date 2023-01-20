ab8bc74205affcd91420d6d1b5a4e1e9426a3f43
leonardogoandete <leonardogoandete>
2023-01-19 22:19:42 -0300

    Configurado service ECS

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -2,13 +2,13 @@ module "ecs" {
   source = "terraform-aws-modules/ecs/aws"
 
   name = var.ambiente
-  
+
   container_insights = true
   capacity_providers = ["FARGATE"]
 
   default_capacity_provider_strategy = [
     {
-        capacity_provider = "FARGATE"
+      capacity_provider = "FARGATE"
     }
   ]
 }
@@ -20,21 +20,34 @@ resource "aws_ecs_task_definition" "Django-api" {
   cpu                      = 256
   memory                   = 512
   execution_role_arn       = aws_iam_role.cargo.arn
-  container_definitions    = jsonencode(
-[
-  {
-    "name" = "producao"
-    "image" = "447542562697.dkr.ecr.us-west-2.amazonaws.com/producao:v1"
-    "cpu" = 256
-    "memory" = 512
-    "essential" = true
-    "portMappings" = [
+  container_definitions = jsonencode(
+    [
       {
-        "containerPort" = 8000
-        "hostPort" = 8080
+        "name"      = "producao"
+        "image"     = "447542562697.dkr.ecr.us-west-2.amazonaws.com/producao:v1"
+        "cpu"       = 256
+        "memory"    = 512
+        "essential" = true
+        "portMappings" = [
+          {
+            "containerPort" = 8000
+            "hostPort"      = 8080
+          }
+        ]
       }
     ]
+  )
+}
+
+resource "aws_ecs_service" "Django-API" {
+  name            = "django-api"
+  cluster         = module.ecs.cluster_id
+  task_definition = aws_ecs_task_definition.Django-api.arn
+  desired_count   = 3
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.alvo.arn
+    container_name   = "producao"
+    container_port   = 8000
   }
-]
-)
 }
