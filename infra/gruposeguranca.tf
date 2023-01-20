8021a55be77ef17fa4dbc93f078ffde44c1b3513
leonardogoandete <leonardogoandete>
2023-01-19 21:03:27 -0300

    Implementado SG e Policies

---
+++ infra/gruposeguranca.tf
@@ -0,0 +1,46 @@
+resource "aws_security_group" "alb" {
+  name        = "alb_ecs"
+  vpc_id      = module.vpc.vpc_id
+}
+
+resource "aws_security_group_rule" "tcp_alb" {
+  type              = "ingress"
+  from_port         = 8000
+  to_port           = 8000
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb.id
+}
+
+resource "aws_security_group_rule" "tcp_alb" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb.id
+}
+
+
+resource "aws_security_group" "privado" {
+  name        = "ecs_privado"
+  vpc_id      = module.vpc.vpc_id
+}
+
+resource "aws_security_group_rule" "entrada_ECS" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  source_security_group_id = aws_security_group.alb.id # vem a requisicao do bloco da linha 1
+  security_group_id = aws_security_group.privado.id
+}
+
+resource "aws_security_group_rule" "saida_ECS" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb.id
+}
