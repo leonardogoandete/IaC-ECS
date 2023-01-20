4ef5a4db45692ae85f75f308450a464f5e461fde
leonardogoandete <leonardogoandete>
2023-01-19 21:43:16 -0300

    Configurado Task ECS

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -12,3 +12,29 @@ module "ecs" {
     }
   ]
 }
+
+resource "aws_ecs_task_definition" "Django-api" {
+  family                   = "Django-api"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn       = aws_iam_role.cargo.arn
+  container_definitions    = jsonencode(
+[
+  {
+    "name" = var.ambiente
+    "image" = "447542562697.dkr.ecr.us-west-2.amazonaws.com/producao:v1"
+    "cpu" = 256
+    "memory" = 512
+    "essential" = true
+    "portMappings" = [
+      {
+        "containerPort" = 8000
+        "hostPort" = 8080
+      }
+    ]
+  }
+]
+)
+}
